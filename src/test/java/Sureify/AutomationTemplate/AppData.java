de8497aacdc73eeea4a1a4db0778d8844c0dd24f
MadhuManu-Sureify <60875947+MadhuManu-Sureify@users.noreply.github.com>
2020-03-23 10:58:41 +0530

    Updated files as per the review comments

---
+++ src/test/java/Sureify/AutomationTemplate/AppData.java
@@ -0,0 +1,31 @@
+package Sureify.AutomationTemplate;
+
+/**
+ * @author Madhu Babu
+ * @date 11-Mar-2020
+ * @desc This Class is to have the Elements locators 
+ */
+
+public class AppData 
+{
+	public static String noBtn = "//div[text()= 'No']";
+	public static String yesBtn = "//div[text()= 'Yes']";
+	public static String nextBtn = "//button[text()='Next']";
+	public static String backBtn = "//button[text()='Back']";
+	public static String saveBtn = "//button[text()='Save']";
+	public static String continueBtn = "//button[text()='Continue']";
+	public static String previousBtn = "//button[text()='Previous']";
+	
+	public static String sessionExpLbl = "//*[text()='Your Session Has Expired']";
+	public static String serverDownLbl = "//*[text()='Please Visit Us Again']";
+
+	public static String docSignAcceptCheckBoxID = "disclosureAccepted";
+	public static String docSignContinueBtnID = "action-bar-btn-continue";
+	public static String docSignStartBtn = "//span[text()='Start']";
+	public static String docSignDownloadIcon = "//*[@class='tab-image tab-image-for-signature']";
+	public static String docSignAdoptSignPopUp = "//span[text()='Adopt Your Signature']";
+	public static String docSignAdoptSignPopUpAdoptSignBtn = "//button[text()='Adopt and Sign']";
+	public static String docSignAdoptSignPopUpCancelBtn = "//button[text()='Cancel']";
+	public static String docSignAdoptSignPopUpFinishBtn = "//button[text()='Finish']";
+	
+}
---
+++ src/test/java/tests/MainTestRunner.java
@@ -0,0 +1,63 @@
+package tests;
+
+/**
+ * @author Madhu Babu
+ * @date 11-Mar-2020
+ * @desc This Class is Main Test runner class to start the test
+ */
+
+import org.testng.annotations.Test;
+
+import Sureify.AutomationTemplate.Suite;
+import modules.LandingPage;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class MainTestRunner {
+  
+	
+	@Test
+  public void f() {
+	  LandingPage l = new LandingPage();
+	  l.tm();
+	  System.out.println("Test Completed");
+  }
+	
+	
+  @BeforeTest
+  public void beforeTest() {
+	  /*
+	   * Start the recorder instance before the every test
+	   */
+	  
+//	 WebActions.startRecorder();	  
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  /*
+	   * Stops the running recording instance after the every test
+	   */
+//	  WebActions.stopRecorder();
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+	  /*
+	   * SuiteUp Driver, WebDriver, Reports 
+	   */
+	  Suite.suitUp();
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+	  /*
+	   * Suite tear down, reports flush
+	   */
+	  Suite.suitTearDown();
+  }
+
+}
